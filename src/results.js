7e032d0f94423d76e0a8ccd5589fe9a069976ac7
basecamp <basecamp@basecamps-MacBook-Pro.local>
2017-09-09 13:37:24 -0700

    v4.3

--- src/results.js
+++ src/results.js
@@ -1,25 +1,25 @@
 
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-import "../public/assets/css/main.css" ; 
+
 
 class Results extends Component {
   render() {
     return (
 			<div className="my-container">
-				<div> <i class="fa fa-trophy" aria-hidden="true"></i>
-					<h3 className='question-text'> Congrats! <br/>Here are your reccomended routers <i class="fa fa-trophy" aria-hidden="true"></i>
+				<div> 
+					<h3 className='question-text'> Congrats! <br/>Here are your reccomended routers 
 					</h3>
 					<h2 className="subquestion-text"> 
 						<ol>
-							<li> If you still have a handful to choose from, consider the router warranty and support. Link to Customer Support table to show differences. </li>
-							<li> If you do not see any routers, you will need a higher price point for the features you need.  					
+							<li>If you still have a handful to choose from, consider the router warranty period and support offerings. Link to Customer Support table to show differences. </li>
+							<li>If you do not see any routers, you will need a higher price point for the features you need.  					
 								</li> 
 						</ol>
 					</h2>
  
-						<Link to="/FrequencyQuestion">
-							<div className='row answer-row' >
+						<Link to="/">
+							<div className='answer-row' >
 								<button className="my-button my-big" onClick={this.props.removeProgress} >
 									Back  
 								</button>
